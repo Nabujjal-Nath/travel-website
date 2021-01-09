68e2bbde55f25f0abf3bacd9cc0c635fa851d9a1
nabujjal <nabujjal@gmail.com>
2021-01-09 11:27:33 +0530

    HeroSection Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom' 
+import Home from './components/pages/Home';
 function App() {
   return (
     <div>
       <Router>
+      <Navbar/>
       <Switch>
-        <Navbar/>
+        <Route path='/' exact component={Home}/>
         </Switch>
       </Router> 
     </div>
---
+++ src/components/HeroSection.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../App.css';
+import sample from '../videos/video-1.mp4';
+import { Button } from './Button';
+import './HeroSection.css';
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+        
+
+        <video className='videoTag' autoPlay loop muted>
+            <source src={sample} type='video/mp4' />
+        </video>
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
+            <div className='hero-btns'>
+                <Button
+                    className='btns'
+                    buttonStyle='btn--outline'
+                    buttonSize='btn--large'
+                >
+                    GET STARTED
+               </Button>
+               <Button
+               className='btns'
+               buttonStyle='btn--primary'
+               buttonSize='btn--large'
+           >
+               WATCH TRAILER <i className='far fa-play-circle'/>
+          </Button>
+            </div>
+        </div>
+    );
+}
+export default HeroSection;
